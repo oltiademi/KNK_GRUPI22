1dc542a8d91c3de54d61f1d9de5add2ba1d24f55
oltiademi <olti.ademi@student.uni-pr.edu>
2023-03-31 00:30:52 +0200

    Initial commit

---
+++ src/main/java/com/example/knk_grupi22/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.knk_grupi22;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk_grupi22 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.knk_grupi22 to javafx.fxml;
+    exports com.example.knk_grupi22;
+}
