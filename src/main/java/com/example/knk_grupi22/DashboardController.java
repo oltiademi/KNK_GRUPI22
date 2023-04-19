0b9ac480313fc2e7f44703502bc9720bf74b96c8
oltiademi <olti.ademi@student.uni-pr.edu>
2023-04-19 11:39:51 +0200

    Started working on the sign up page

--- src/main/java/com/example/knk_grupi22/DashboardController.java
+++ src/main/java/com/example/knk_grupi22/DashboardController.java
@@ -1,4 +1,42 @@
 package com.example.knk_grupi22;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardController {
+    @FXML
+    private Button logout_Btn;
+
+    public void logout(ActionEvent actionEvent) throws IOException {
+        if(actionEvent.getSource() == logout_Btn){
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+            Parent homeRoot = fxmlLoader.load();
+
+            // Create a FXMLLoader for the dashboard.fxml file
+            FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+            Parent dashboardRoot = fxmlLoader1.load();
+
+            // Create a Scene for the dashboard
+            Scene scene = new Scene(dashboardRoot, 637, 425);
+
+            // Create a Stage for the dashboard
+            Stage stage = new Stage();
+            stage.setScene(scene);
+
+            // Hide the current window
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.hide();
+
+            // Show the dashboard window
+            stage.show();
+        }
+    }
 }
--- src/main/java/com/example/knk_grupi22/LoginController.java
+++ src/main/java/com/example/knk_grupi22/LoginController.java
@@ -3,10 +3,16 @@ package com.example.knk_grupi22;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -21,8 +27,38 @@ public class LoginController {
     @FXML
     private TextField tf_Username;
 
-    public void switchForm(ActionEvent actionEvent){
+    public void switchForm(ActionEvent actionEvent) throws IOException {
         if(actionEvent.getSource() == login_Btn){
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+            Parent homeRoot = fxmlLoader.load();
+
+
+            FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+            Parent dashboardRoot = fxmlLoader1.load();
+
+            // Create a Scene for the dashboard
+            Scene scene = new Scene(dashboardRoot, 1540, 790);
+
+            // Create a Stage for the dashboard
+            Stage stage = new Stage();
+            stage.setScene(scene);
+
+            // Hide the current window
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.hide();
+
+            // Show the dashboard window
+            stage.show();
         }
+    }
+    public void signUp(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sign-up.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 750, 400);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.hide();
     }
 }
